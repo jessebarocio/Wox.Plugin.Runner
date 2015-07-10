dce49414dcdd414f2475bd152526de23dc20e841
Jesse Barocio <jesse@silverspheredev.com>
2015-07-10 15:27:24 -0600

    Created a "Hello World" plugin.

---
+++ Wox.Plugin.Runner/Runner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wox.Plugin.Runner
+{
+    public class Runner : IPlugin
+    {
+        public void Init( PluginInitContext context )
+        {
+            
+        }
+
+        public List<Result> Query( Query query )
+        {
+            var results = new List<Result>();
+            results.Add( new Result
+                {
+                    Title = "Hello world!",
+                    SubTitle = "You've successfully installed this plugin.",
+                    IcoPath = "Images\\globe.png",
+                    Action = e =>
+                        {
+                            return false;
+                        }
+                } );
+            return results;
+        }
+    }
+}
