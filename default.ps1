954b6de4bd4b9d0587a2c5582cb255d2c6dc3d70
Jesse Barocio <jesse@silverspheredev.com>
2015-07-21 16:12:26 -0600

    Auto-generate plugin.json manifest on package.

--- default.ps1
+++ default.ps1
@@ -9,6 +9,8 @@ properties {
 	$nunitDir = ".\packages\NUnit.Runners.2.6.3\tools"
 	$openCoverDir = ".\packages\OpenCover.4.5.2506"
 	$reportGeneratorDir = ".\packages\ReportGenerator.1.9.1.0"
+        
+    $releasePath = "$baseDir\Wox.Plugin.Runner\bin\Release"
 }
 
 Task Default -depends Build
@@ -29,14 +31,33 @@ Task IncrementVersion {
     $regex = New-Object System.Text.RegularExpressions.Regex("([0-9]+\.){3}[0-9]+")
     $version = $regex.Matches($rawContent)[0].Value
     
-    $replaceRegex = New-Object System.Text.RegularExpressions.Regex("(?<=([0-9]+\.[0-9]+\.))[0-9](?=\.[0-9])")
+    $replaceRegex = New-Object System.Text.RegularExpressions.Regex("(?<=([0-9]+\.[0-9]+\.))[0-9]+(?=\.[0-9])")
     $newVersion = $replaceRegex.Replace($version, ([int]($replaceRegex.Match($version).Value) + 1).ToString())
     
     $rawContent.Replace($version, $newVersion).Trim() | Out-File $assemblyInfo -Encoding UTF8
 }
 
-Task Package -depends Release {
-    $releasePath = "$baseDir\Wox.Plugin.Runner\bin\Release"
+Task GenerateManifest {
+    $primaryAssembly = Get-Item "$baseDir\Wox.Plugin.Runner\bin\Release\Wox.Plugin.Runner.dll" | select -ExpandProperty FullName
+    
+    $assembly = [Reflection.Assembly]::LoadFile($primaryAssembly)
+    $manifestTemplate = Get-Content "$baseDir\plugin.json.template" -Raw
+
+    $title = $assembly.GetName().Name
+    $description = $assembly.GetCustomAttributes([System.Type]::GetType("System.Reflection.AssemblyDescriptionAttribute"), $false)[0].Description
+    $company = $assembly.GetCustomAttributes([System.Type]::GetType("System.Reflection.AssemblyCompanyAttribute"), $false)[0].Company
+    $version = $assembly.GetName().Version
+    $packageVersion = "$($version.Major).$($version.Minor).$($version.Build)"
+    
+    $manifestTemplate = $manifestTemplate.Replace("#title#", $title)
+    $manifestTemplate = $manifestTemplate.Replace("#description#", $description)
+    $manifestTemplate = $manifestTemplate.Replace("#author#", $company)
+    $manifestTemplate = $manifestTemplate.Replace("#version#", $packageVersion)
+    
+    $manifestTemplate | Out-File "$releasePath\plugin.json" -Encoding UTF8
+}
+
+Task Package -depends IncrementVersion, Release, GenerateManifest {
     pushd $releasePath
     $packageFiles = ls -Recurse -Exclude Wox.Plugin.dll,*.pdb -Attributes !D
     Zip $packageFiles "$baseDir\Wox.Plugin.Runner.wox"
